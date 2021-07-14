ee5aa35e7b3952eec39b1b4a705b08003ab9f2d0
James Perlichek <jamesperlichek15@gmail.com>
2021-07-14 15:21:51 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,16 +11,16 @@ class TestEnglishToFrench(unittest.TestCase):
     Class to handle all testing of the English to French translator
     """
     def test1(self):
-        #Test the basic 'Hello' greeting
+        """Test the basic 'Hello' greeting"""
         self.assertEqual(englishtofrench('Hello'), 'Bonjour')
 
     def test2(self):
-        #Test a more complex sentence
+        """Test a more complex sentence"""
         self.assertEqual(englishtofrench(\
             'What are you doing today?'), "Que faites-vous aujourd'hui?")
 
     def test3(self):
-        #Test for null input
+        """Test for null input"""
         self.assertRaises(ValueError, englishtofrench, None)
 
 class TestEnglishToGerman(unittest.TestCase):
@@ -28,16 +28,15 @@ class TestEnglishToGerman(unittest.TestCase):
     Class to handle testing of the English to German translator
     """
     def test1(self):
-        #Test the basic 'Hello' greeting
+        """Test the basic 'Hello' greeting"""
         self.assertEqual(englishtogerman('Hello'), 'Hallo')
 
     def test2(self):
-        #Test a more complex sentence
+        """Test a more complex sentence"""
         self.assertEqual(englishtogerman(\
             'Do you speak German?'), "Sprechen Sie Deutsch?")
 
     def test3(self):
-        #Test for null input
+        """Test for null input"""
         self.assertRaises(ValueError, englishtogerman, None)
-        
 unittest.main()
