38199559f9498284bde8b7177f4208e3dfa0c72d
James Perlichek <jamesperlichek15@gmail.com>
2021-07-14 12:39:04 -0500

    Update translator.py

--- translator.py
+++ translator.py
@@ -23,3 +23,20 @@ def englishtofrench(recognized_text):
         text=recognized_text, model_id='en-fr')
     translation=translation_response.get_result()
     return translation['translations'][0]['translation']
+
+def englishtogerman(recognized_text):
+    #Define authentication components in order to use IBM Watson Language Translator
+    url = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/55dac344-a3eb-4394-b60c-88c078032048'
+    api_key = 'v0KOYgPO7z39nxPElAHvpVY-CpMXZOkagfLUoq_TPmak'
+    version = '2018-05-01'
+
+    #Creating the language translator object
+    authenticator = IAMAuthenticator(api_key)
+    language_translator = LanguageTranslatorV3(version=version, authenticator=authenticator)
+    language_translator.set_service_url(url)
+
+    #Create the actual translated response as a string
+    translation_response = language_translator.translate(\
+        text=recognized_text, model_id='en-de')
+    translation=translation_response.get_result()
+    return translation['translations'][0]['translation']
